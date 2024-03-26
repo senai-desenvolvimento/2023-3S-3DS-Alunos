c5a716bb30442f734abcf609e09065d21832c0d5
Carlos Roque <carlos.roque@info.local>
2024-03-26 10:01:04 -0300

    alterado estrutura banco e adicionado metodos

---
+++ API-VitalHub/Scripts/script_banco_alterado.sql
@@ -0,0 +1,37 @@
+USE VitalHub;
+
+-- Select de Tipo Usuario
+SELECT * FROM dbo.TiposUsuario;
+
+-- Select de Usuario
+SELECT * FROM dbo.Usuarios;
+
+-- Select de Pacientes
+SELECT * FROM dbo.Pacientes;
+
+-- Select de Medicos
+SELECT * FROM dbo.Medicos;
+
+-- Select de Especialidade
+SELECT * FROM dbo.Especialidades;
+
+-- Select de Medicos Clinicas
+SELECT * FROM dbo.MedicosClinicas;
+
+-- Select de Endereços
+SELECT * FROM dbo.Enderecos;
+
+-- Select de Clinicas
+SELECT * FROM dbo.Clinicas;
+
+-- Select de Situacoes
+SELECT * FROM dbo.Situacoes;
+
+-- Select de Niveis de prioridade
+SELECT * FROM dbo.NiveisPrioridade;
+
+-- Select de Exames
+SELECT * FROM dbo.Exames;
+
+-- Select de Consultas
+SELECT * FROM dbo.Consultas;

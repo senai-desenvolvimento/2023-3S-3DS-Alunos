ce57eb0263b33f2fa706cbb7a96fcea5100ab868
Carlos Roque <carlos.roque@info.local>
2024-04-10 13:57:49 -0300

    atualiza api

--- API-VitalHub/WebAPI/WebAPI/Utils/Mail/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebAPI.Utils.Mail
-{
-    public interface IEmailService
-    {
-        //método asincrono para envio de email que recebe MailRequest
-        Task SendEmailAsync(MailRequest mailRequest);
-    }
-}
